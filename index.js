31b3e31a448622f27cc7236343539398f5b576aa
giomadeira <giomadeirag@gmail.com>
2020-11-07 11:17:44 -0300

    atualização da pasta com a manipulação do DOM.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const nomeCodificar = document.getElementById('trazerCodigo')
+nomeCodificar.addEventListener('click',trazerCodigo)
+ 
+function trazerCodigo(clickGerarCodigo){
+clickGerarCodigo.preventDefault()
+let nome = document.getElementById('nome').value;
+nome = nome.toUpperCase();
+let desloca = Number(document.getElementById('offset').value)
+const nomeCodificado = cipher.encode(desloca,nome);
+document.getElementById("codigoGerado").value = `${nomeCodificado}`;
+} 
+const trazerNome = document.getElementById('reverteCodigo')
+trazerNome.addEventListener('click',reverteCodigo)
+ 
+function reverteCodigo(clickGerarNome){
+clickGerarNome.preventDefault()
+let decodifica = document.getElementById('codigoAsc').value;
+decodifica = decodifica.toUpperCase();
+let volta = Number(document.getElementById('offset2').value)
+const nomeDecifrado = cipher.decode(volta,decodifica);
+document.getElementById("nomeUsuario").value = `${nomeDecifrado}`;
+  
+ 
+} 
+
